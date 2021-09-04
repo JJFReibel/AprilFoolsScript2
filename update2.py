feadc40b37e13ecced60728ecc86760fa9bdac9a
Jean-Jacques François Reibel <9219877+JJFReibel@users.noreply.github.com>
2021-09-04 11:18:27 -0700

    uploaded update2.py

---
+++ update2.py
@@ -0,0 +1,49 @@
+# April Fools Script 2
+# By Jean-Jacques F. Reibel
+# I will not be held responsible for:
+# any shenanigans
+import os
+
+#                     ಠ_ಠ
+
+#                     ¯\_(ツ)_/¯
+
+os.system("printf '\e[0;31;1;1m             ________\n'")
+os.system("printf '\e[0;31;1;1m            /        \\\n'")
+os.system("printf '\e[0;31;1;1m           /          \\\n'")
+os.system("printf '\e[0;31;1;1m          /            \\\n'")
+os.system("printf '\e[0;31;1;1m         |              |\n'")
+os.system("printf '\e[0;31;1;1m         |'")
+os.system("printf '\e[0;37;1;1m     STOP'")
+os.system("printf '\e[0;31;1;1m     |\n'")
+
+os.system("printf '\e[0;31;1;1m         |              |'")
+os.system("printf '\e[0;31;1;5m                        ~'")
+os.system("printf '\e[0;31;1;1m>'")
+os.system("printf '\e[0;33;1;1m°'")
+os.system("printf '\e[1;32;1;1m)～～～～～～～～～～  \n'")
+os.system("printf '\e[0;31;1;1m          \\            /'")
+os.system("printf '\e[1;32;1;1m                                                  \\\n'")
+os.system("printf '\e[0;31;1;1m           \\          /'")
+os.system("printf '\e[1;32;1;1m                                                    |\n'")
+os.system("printf '\e[0;31;1;1m            \________/\'")
+os.system("printf '\e[1;32;1;1m                                                    /\n'")
+os.system("printf '\e[0;37;1;1m  ┌─┐     ┌─┐     ┌─┐     ┌─┐     ┌─┐'")
+os.system("printf '\e[1;32;1;1m                       ～～～～～～～    \\\n'")
+os.system("printf '\e[0;37;1;1m  | |     | |     | |     | |     | |'")
+os.system("printf '\e[1;32;1;1m                     /                \\   |\n'")
+os.system("printf '\e[0;37;1;1m -┴─┴-   -┴─┴-   -┴─┴-   -┴─┴-   -┴─┴-'")
+os.system("printf '\e[1;34;1;5m  WE OWN U'")
+os.system("printf '\e[1;32;1;0m    $200 |～～～～～～～～～|  |\n'")
+os.system("printf '\e[0;37;1;1m (╭ರ_•́)  (╭ರ_•́)  (╭ರ_•́)  (╭ರ_•́)   ಠ_ಠ'")
+os.system("printf '\e[0;37;1;1m      '")
+os.system("printf '\e[0;37;1;1m¯'")
+os.system("printf '\e[0;31;1;1m\_'")
+os.system("printf '\e[0;33;1;1m('")
+os.system("printf '\e[0;34;1;1mツ'")
+os.system("printf '\e[0;33;1;1m)'")
+os.system("printf '\e[0;31;1;1m_/'")
+os.system("printf '\e[0;37;1;1m¯'")
+os.system("printf '\e[1;32;1;1m     \\                /  /\n'")
+os.system("printf '\e[1;32;1;1m                                                            ～～～～～～～～～'")
+os.system("printf '\e[0;33;1;1m\n'")
